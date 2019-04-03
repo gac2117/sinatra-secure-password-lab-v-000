85bed29699f4544bf0f88904f988bffcd31475ea
Grace Choi <oiograceoio@gmail.com>
2019-04-03 18:26:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,8 @@ class ApplicationController < Sinatra::Base
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect to '/account'
+      else
+        redirect '/failure'
       end
     end
   end
